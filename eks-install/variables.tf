20bc619b1a507bf970ee1414e7fc9caeb51e5457
Mraakhil <mdaakhil888@gmail.com>
2026-07-23 20:54:02 +0530

    Update EKS variables for region, availability zones, and instance types

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
 
 variable "private_subnet_cidrs" {
@@ -53,10 +53,10 @@ variable "node_groups" {
   }))
   default = {
     general = {
-      instance_types = ["t3.medium"]
+      instance_types = ["c7i-flex.large"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
-        desired_size = 2
+        desired_size = 1
         max_size     = 4
         min_size     = 1
       }
