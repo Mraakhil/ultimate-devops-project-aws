e3fb6f86b7d012b75117c8472605cc516e0c5666
Mraakhil <mdaakhil888@gmail.com>
2026-07-24 10:29:56 +0530

    yes

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -9,8 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "demo-terraform-eks-state-s3-bucket"
     key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-eks-state-locks"
+    region         = "us-west-2"
     encrypt        = true
   }
 }
